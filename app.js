1ba2f2342fad273092295e0cd4adfa01dd5f04c4
Naman Dangi <namandangi@users.noreply.github.com>
2019-09-27 09:19:31 +0530

    Homepage & Scholarship page setup

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const   express          = require('express'),
+        bodyParser       = require('body-parser'),
         app              = express();
 
 
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.set("view engine", "ejs");
+
 
         app.get('/',(req,res)=>{
-            res.send('HEY THERE !');
+            res.render('home');
+        });
+
+        app.get('/s',(req,res)=>{
+            res.render('scholarships');
+        });
+
+        app.get('*',(req,res)=>{
+            res.redirect('/');
         });
 
         app.listen(process.env.PORT||3000,process.env.IP,()=>{
